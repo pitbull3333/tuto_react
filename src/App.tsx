913b7b21677013c5881c536e74884b3fa94968c5
unknown <xthieuleuxjeux@gmail.com>
2025-04-29 17:08:59 +0200

    commentaire

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,31 @@ const default_tweet = [
     },
 ]
 function App(){
+    const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const formData = new FormData(event.currentTarget);
+        const name = formData.get("name") as string;
+        const newTweet = {
+            id:tweets.length > 0 ? tweets[0].id + 1:0,
+            name,
+            like:0,
+        }
+        console.log({newTweet});
+    }
     const [tweets,setTweets] = useState(default_tweet);
     const onDelete = (tweetId:number) =>{
         setTweets((curr) => curr.filter((tweet) => tweet.id !== tweetId));
     };
     return (
         <div>
+            <div className="tweet_form">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="name" placeholder="name" />
+                    {/*<input type="content" name="content" placeholder="name" />*/}
+                    <input type="submit" />
+                </form>
+            </div>
             <div className="tweet_contenaire">
                 {tweets.map((tweet) => {
                     return (
@@ -38,9 +57,7 @@ function App(){
                             id={tweet.id}
                             name ={tweet.name}
                             like={tweet.like}
-                            onDelete = {(id) => {
-                                onDelete(id);
-                            }}
+                            onDelete={onDelete}
                         />
                     );
                 })}
