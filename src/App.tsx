4bdca18baa28d27f810ea7b724466b9afc97e009
unknown <xthieuleuxjeux@gmail.com>
2025-05-15 16:55:00 +0200

    comentaire

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {HashRouter as Router,Routes,Route} from 'react-router-dom';
 import Profil from './Profil';
 import Accueil from './Accueil';
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/profil" element={<Profil />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
